d728667f82373a4de24695ecf7b79609dd538982
Ok <epicfail@gmx.fr>
2012-10-12 23:19:28 +0200

    Code du conteneur de tâche en cours, pas mal de TODO encore (dont libération mémoire)

--- ordonnanceur/ConteneurTache.cpp
+++ ordonnanceur/ConteneurTache.cpp
@@ -5,5 +5,23 @@ ConteneurTache::ConteneurTache() {
 }
 
 ConteneurTache::~ConteneurTache() {
+	// TODO: Libérer la  mémoire (tâches ajoutées dynamiquement /!\)
+}
+
+void ConteneurTache::addTachePeriodique(int Ci, int Pi, int Di) {
+	TachePeriodique* tacheP = new TachePeriodique(Ci, Pi, Di);
+	cout << "Creation de la tâche T : " << tacheP->toString() << endl;
+	//tabTaches_.push_back(*tacheP);
+}
+
+void ConteneurTache::addTacheAperiodique(int ri, int Ci) {
+
+}
+
+void ConteneurTache::deleteTache(TachePeriodique tache) {
+
+}
+
+void ConteneurTache::deleteTache(TacheAperiodique tache) {
 
 }
--- ordonnanceur/ConteneurTache.h
+++ ordonnanceur/ConteneurTache.h
@@ -1,8 +1,28 @@
+#ifndef _CONTENEURTACHE_H_
+#define _CONTENEURTACHE_H_
+
+#include <vector>
+#include <iostream>
+
 #include "TacheAperiodique.h"
 #include "TachePeriodique.h"
+
+using namespace std;
+
+typedef vector<Tache> TabTaches;
 
 class ConteneurTache {
 public:
 	ConteneurTache();
 	~ConteneurTache();
+	
+	void addTachePeriodique(int Ci, int Pi, int Di);
+	void addTacheAperiodique(int ri, int Ci);
+	void deleteTache(TachePeriodique tache);
+	void deleteTache(TacheAperiodique tache);
+	
+private:
+	TabTaches tabTaches_;
 };
+
+#endif
