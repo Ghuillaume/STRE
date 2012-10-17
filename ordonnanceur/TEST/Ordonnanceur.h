28b4455cd9771058479683b1f85f4330d5cf6021
Ok <epicfail@gmx.fr>
2012-10-17 17:25:39 +0200

    Merge des calculs d'ordonnançabilité + correction de quelques erreurs. PS : Il en reste une tonne encore à corriger (en particulier dans OrdonnancementEDF()

--- ordonnanceur/TEST/Ordonnanceur.h
+++ ordonnanceur/TEST/Ordonnanceur.h
@@ -1,6 +1,9 @@
 #ifndef ORDONNANCEUR_H
 #define ORDONNANCEUR_H
+
 #include <iostream>
+#include <cmath>
+
 #include "ConteneurTachePeriodique.h"
 
 using namespace std;
@@ -10,12 +13,16 @@ class Ordonnanceur {
 		Ordonnanceur();
 		~Ordonnanceur();
 		void OrdonnancementRM(ConteneurTachePeriodique* conteneur);
+		void OrdonnancementEDF(ConteneurTachePeriodique* conteneur);
+		void verifierOrdonnancabilite(ConteneurTachePeriodique* conteneur);
 		
 	private:
-	
 		ConteneurTachePeriodique* conteneur_;
 		
-
+		static void verifierCondNecessaireRM(ConteneurTachePeriodique* conteneur);
+		static void verifierCondSuffisanteRM(ConteneurTachePeriodique* conteneur);
+		static double calculerUp(ConteneurTachePeriodique* conteneur);
+		static double calculerUBound(double n);
 };
 #endif
 
