474f48c7eb47f006236bb89badf97f94d50175a7
Guillaume <guillaume@guillaume-desktop.(none)>
2012-10-29 17:51:47 +0100

    Finalisation du Parseur wwahouu

---
+++ ordonnanceur/common.c
@@ -0,0 +1,9 @@
+#include "common.h"
+
+ListOfString explode(const std::string& str, const char& delimiter)
+{
+    std::istringstream split(str);
+    std::vector<std::string> tokens;
+    for (std::string each; std::getline(split, each, delimiter); tokens.push_back(each));
+    return tokens;
+}
