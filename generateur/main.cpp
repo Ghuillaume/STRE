221c0b19f0d08c201bddde17280b756074576b6f
Ok <yoros@gmx.fr>
2012-11-14 14:31:27 +0100

    Modifs lancement Generateur + rapport analyse pb Generateur

--- generateur/main.cpp
+++ generateur/main.cpp
@@ -8,62 +8,57 @@ Auteur ...... : BONCORPS, CHARON, PAGES
 #include "GenerateurControlee.hpp"
 
 using namespace std;
-int main(int argc, char** argv) {
-	string USAGE = "./Generateur [-f fichier.txt|-c nbTaches|-a nbTaches utilisationCPU]";
-	// Vérification du nombre d'arguments
+int main(int argc, char** argv) {	
+	char choix;
+	int nbTachesP = 0;
+	int nbTachesA = 0;
+	int utCPUP = 0;
+	int utCPUA = 0;
 	
-	if (argc == 1) {
-		char choix;
-		int nbTachesP = 0;
-		int nbTachesA = 0;
-		int utCPUP = 0;
-		int utCPUA = 0;
-		//interface de selection
-		cout << endl << "Choix de Generation :" << endl;
-		cout << "	1 : Generation Aleatoire" << endl;
-		cout << "	2 : Generation Controlee" << endl;
-		cout << "Selection : ";
-		cin >> choix;
-		if (choix == '1') {
-			// Generation Aleatoire
-			cout << endl << endl << "Nombre de Taches Periodiques: ";
-			cin >> nbTachesP;
-			if(nbTachesP == 0) {
-				cout << "ATTENTION : l'application doit générer au moins une tache périodique" << endl;
-				exit(0);
-			}
-			cout << "Pourcentage d'Utilisation du CPU (periodique): ";
-			cin >> utCPUP;
-			cout << endl << endl << "Nombre de Taches Aperiodiques: ";
-			cin >> nbTachesA;
-			if(nbTachesA != 0) {
-				cout << "Pourcentage d'Utilisation du CPU (aperiodique): ";
-				cin >> utCPUA;
-			}
-			else {
-				utCPUA = 0;
-			}
-			
-			GenerateurAleatoire genAl(nbTachesP,nbTachesA,utCPUP,utCPUA);
+	//interface de selection
+	cout << endl << "Choix de Generation :" << endl;
+	cout << "	1 : Generation Aleatoire" << endl;
+	cout << "	2 : Generation Controlee" << endl;
+	cout << "Selection : ";
+	cin >> choix;
+	
+	if (choix == '1') {
+		// Generation Aleatoire
+		cout << endl << endl << "Nombre de Taches Periodiques: ";
+		cin >> nbTachesP;
+		if(nbTachesP == 0) {
+			cout << "ATTENTION : l'application doit générer au moins une tache périodique" << endl;
+			exit(0);
 		}
-		else if(choix == '2') {
-			// Generation Controlee
-			cout << endl << endl << "Nombre de Taches Periodiques: ";
-			cin >> nbTachesP;
-			if(nbTachesP == 0) {
-				cout << "ATTENTION : l'application doit générer au moins une tache périodique" << endl;
-				exit(0);
-			}
-			cout << endl << endl << "Nombre de Taches Aperiodiques: ";
-			cin >> nbTachesA;
-			GenerateurControlee genContr(nbTachesP,nbTachesA);
+		cout << "Pourcentage d'Utilisation du CPU (periodique): ";
+		cin >> utCPUP;
+		cout << endl << endl << "Nombre de Taches Aperiodiques: ";
+		cin >> nbTachesA;
+		if(nbTachesA != 0) {
+			cout << "Pourcentage d'Utilisation du CPU (aperiodique): ";
+			cin >> utCPUA;
 		}
 		else {
-			cout << "erreur";
+			utCPUA = 0;
 		}
-	}	
-	else
-		cout << "Arguments incorrects.\n" << USAGE << endl;
+		
+		GenerateurAleatoire genAl(nbTachesP,nbTachesA,utCPUP,utCPUA);
+	}
+	else if(choix == '2') {
+		// Generation Controlee
+		cout << endl << endl << "Nombre de Taches Periodiques: ";
+		cin >> nbTachesP;
+		if(nbTachesP == 0) {
+			cout << "ATTENTION : l'application doit générer au moins une tache périodique" << endl;
+			exit(0);
+		}
+		cout << endl << endl << "Nombre de Taches Aperiodiques: ";
+		cin >> nbTachesA;
+		GenerateurControlee genContr(nbTachesP,nbTachesA);
+	}
+	else {
+		cout << "Erreur : choix non valide. \n(Rappel : 1 pour la génération aléatoire ou 2 pour la génération contrôlée)\n";
+	}
 }
 
 
